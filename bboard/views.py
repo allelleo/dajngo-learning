6193d385f08787ef43844e38046d74209fdc5f5b
allelleo <dev.allelleo@internet.ru>
2022-12-08 20:24:10 +0300

    commit 2

--- bboard/views.py
+++ bboard/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from .models import Bb
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello from allelleo")
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {
+        'bbs': bbs,
+    }
+    return HttpResponse(template.render(context, request))
